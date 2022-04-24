6f06b81600c52f84a86743f3c79a8f3608d6f3b9
ksuess <k.suess@rohberg.ch>
2022-04-24 21:49:13 +0200

    Remove redundant code of codeblock parser.

--- src/manuel/myst/codeblock.py
+++ src/manuel/myst/codeblock.py
@@ -1,19 +1,15 @@
 import re
 import textwrap
 
-import manuel
+from manuel import Manuel as BaseManuel
+from manuel.codeblock import CodeBlock, execute_code_block
+
 
 CODEBLOCK_START = re.compile(
     r"((^```python)|(^% invisible-code-block:\s+python)$)",
     re.MULTILINE,
 )
 CODEBLOCK_END = re.compile(r"(\n(?=```\n))|((?:% [\S ]*)\n(?=\n))")
-
-
-class CodeBlock(object):
-    def __init__(self, code, source):
-        self.code = code
-        self.source = source
 
 
 def find_code_blocks(document):
@@ -28,14 +24,6 @@ def find_code_blocks(document):
         region.parsed = CodeBlock(code, source)
 
 
-def execute_code_block(region, document, globs):
-    if not isinstance(region.parsed, CodeBlock):
-        return
-
-    exec(region.parsed.code, globs)
-    del globs["__builtins__"]  # exec adds __builtins__, we don't want it
-
-
-class Manuel(manuel.Manuel):
+class Manuel(BaseManuel):
     def __init__(self):
-        manuel.Manuel.__init__(self, [find_code_blocks], [execute_code_block])
+        BaseManuel.__init__(self, [find_code_blocks], [execute_code_block])
